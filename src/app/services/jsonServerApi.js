b367e48bb9dc8a06784d08beaba8ec4791c56827
Raynaldo Sutisna <raaynaldo@gmail.com>
2022-11-25 23:18:11 -0500

    feat: add providesTags and invalidatesTags

--- src/app/services/jsonServerApi.js
+++ src/app/services/jsonServerApi.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const jsonServerApi = createApi({
   reducerPath: 'jsonServerApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/' }),
+  tagTypes: ['Albums'],
   endpoints: (builder) => ({
     getAlbums: builder.query({
       query: (page = 1) => `albums?_page=${page}&_limit=10`,
+      providesTags: ['Albums'],
     }),
 
     createAlbum: builder.mutation({
@@ -14,6 +16,7 @@ export const jsonServerApi = createApi({
         method: 'POST',
         body: { title },
       }),
+      invalidatesTags: ['Albums'],
     }),
   }),
 });
